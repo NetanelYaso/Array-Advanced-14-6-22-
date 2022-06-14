75c82d76461cf621a49b6149fcbde03ee2beaeb5
NetanelYaso <netanelyaso12@gmail.com>
2022-06-14 19:52:33 +0300

    array-1

---
+++ main.js
@@ -0,0 +1,64 @@
+// function printStringArray(myArray) {
+//     myArray.sort();
+//     console.log(myArray);
+// }
+// printStringArray(["yellow", "red", "blue", "pink", "black"]);
+
+// function numbersArray(numsArray) {
+//     numsArray.sort((a,b)=> a-b);
+//     console.log(numsArray);
+// }
+// numbersArray([5,6,8,1,2,7,3]);
+
+// function numsArray(numArray) {
+//     numArray.sort((a,b)=>b-a);
+//     console.log(numArray);
+// }
+// numsArray([5,86,8,14,2,73,356]);
+
+// let myArray = [];
+// function names(){
+// let myNames = document.getElementById("input_text").value;
+//   if(myArray.length<10){
+//     myArray.push(myNames);
+//   }   
+//   myArray.sort();
+//   console.log(myArray);
+// }
+
+// let array =[];
+// function rndArray(){
+//   for(let i =0; i<10; i++){
+//     let rndNumber = Math.floor(Math.random()*100);
+//     array.push(rndNumber);
+//   }
+//   array.sort((a,b)=> b-a);
+//   console.log(array);
+// }
+// rndArray();
+
+// let myArray = [];
+// function changeArrayToString(){
+// let myNames = document.getElementById("input_string").value;
+//   if(myArray.length<10){
+//     myArray.push(myNames);
+//   }
+//   let str = myArray.join("-");
+//   console.log(str);   
+// }    
+// changeArrayToString();
+
+
+let techArray = ["Google","Meta","Amazon","Microsoft","Hp","Monday","IT&T","Aplly","Moovite","Waze"];
+function printHighTech() {
+  // let number = 0;
+  for(let i =0; i<techArray.length;i++){
+        my_h2.innerHTML = techArray 
+
+  }
+  // if(number<techArray.length){
+  //   my_h2.innerHTML = +techArray 
+  // }
+  //    console.log(techArray);
+}
+printHighTech()
